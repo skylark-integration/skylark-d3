461e28de4224b9f8bc1a4c5b0991e794339aa6f9
hudaokeji <github@hudaokeji.com>
2020-03-16 11:07:36 +0900

    init commit

---
+++ dist/skylark-d3/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-d3 - A version of d3 that ported to running on skylarkjs
+ * @author Hudaokeji, Inc.
+ * @version v0.9.2
+ * @link https://github.com/skylark-integration/skylark-d3/
+ * @license MIT
+ */
+define(["./d3"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
